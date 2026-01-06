62a0260ef663dcefee2050b29cdd938a363a897a
Ibrahim Megahed <ibrahim__hasan@outlook.com>
2026-01-06 08:59:13 +0300

    simplify operation
    Signed-off-by: Ibrahim Megahed <ibrahim__hasan@outlook.com>

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -43,7 +43,7 @@ public class GildedRose(IList<Item> items)
                     }
                     else
                     {
-                        item.Quality = item.Quality - item.Quality;
+                        item.Quality = 0;
                     }
                 }
                 else
