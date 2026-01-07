d6b5e5b5fb89e1aa0bf96a2e563a32950ffefc69
Ibrahim Megahed <ibrahim__hasan@outlook.com>
2026-01-07 07:57:37 +0300

    rl
    Signed-off-by: Ibrahim Megahed <ibrahim__hasan@outlook.com>

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -53,8 +53,6 @@ public class GildedRose(IList<Item> items)
 
     private void IncreaseBackStagePassesQuality(Item item)
     {
-        if (item.Quality >= 50) return;
-
         item.Quality++;
         if (item.Quality >= 50) return;
 
